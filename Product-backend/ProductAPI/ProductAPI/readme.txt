4c70097f73227c42ff074aea6b963ac883345a83
Ebin Xavier <ebinxavier81@gmail.com>
2026-04-27 14:01:02 +0530

    folder deleted

--- Product-backend/ProductAPI/ProductAPI/readme.txt
+++
@@ -1,2 +0,0 @@
-﻿this is a .net backend application for basic CRUD operations
-frontend is developed with react
---
+++ ProductAPI/ProductAPI/Program.cs
@@ -0,0 +1,56 @@
+﻿
+using Microsoft.EntityFrameworkCore;
+
+namespace ProductAPI
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+
+            builder.Services.AddControllers();
+            builder.Services.AddDbContext<AppDbContext>(options =>
+                options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")));
+            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
+            builder.Services.AddCors();
+
+            var app = builder.Build();
+
+            // Create database
+            using (var scope = app.Services.CreateScope())
+            {
+                var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+                db.Database.EnsureCreated();
+                //try
+                //{
+                //    db.Database.ExecuteSqlRaw("ALTER TABLE Products ADD COLUMN Quantity INTEGER NOT NULL DEFAULT '';");
+                //    Console.WriteLine("✓ Category column added");
+                //}
+                //catch { } // Column might already exist
+            }
+
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseAuthorization();
+
+
+            app.UseCors(x => x.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
+
+            app.MapControllers();
+
+            app.Run();
+        }
+    }
+}
